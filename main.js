65666650c97ba41143e4463250fd369387541bd5
Rob <robhodgskin@gmail.com>
2021-05-17 12:47:52 +0100

    fetch up and running

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 
-const token = 
+const token = "c47rg1zHgNfX4-dBEcDv"
 async function getCharacter () {
-    let response = await fetch ("https://the-one-api.dev/v2/character/1")
-    let data = await response.json()
-    console.log(data);
+    let response = await fetch ("https://the-one-api.dev/v2/character", {    
+    headers: {
+        'Accept': 'application/json',    
+        'Authorization': `Bearer ${token}`
+        }
+    })
+    const data = await response.json();
+    console.log(data.docs[0].name);
 }
 
 getCharacter();
-
 
 

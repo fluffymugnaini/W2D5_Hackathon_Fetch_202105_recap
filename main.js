6043f291bfe53935431b483fa61e6d782b1dff33
Stella Taylor <stellataylor10@gmail.com>
2021-05-17 14:46:30 +0100

    entrance button done and display function done done done

--- main.js
+++ main.js
@@ -1,16 +1,40 @@
+//entrance button
+let entranceButton = document.createElement("button");
+entranceButton.id = "entrance-button";
+entranceButton.innerText = "YOU SHALL PASS";
 
-const token = "c47rg1zHgNfX4-dBEcDv"
-async function getCharacter () {
-    let response = await fetch ("https://the-one-api.dev/v2/character", {    
+let mainDiv = document.querySelector("#container");
+mainDiv.appendChild(entranceButton);
+
+function enter() {
+  entranceButton.style.display = 'none';
+  console.log("hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii");
+}
+
+entranceButton.addEventListener("click", enter);
+
+const token = "c47rg1zHgNfX4-dBEcDv";
+async function getCharacter() {
+  let response = await fetch("https://the-one-api.dev/v2/character", {
     headers: {
-        'Accept': 'application/json',    
-        'Authorization': `Bearer ${token}`
-        }
-    })
-    const data = await response.json();
-    console.log(data.docs[0].name);
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const data = await response.json();
+  console.log(data);
+}
+
+async function getQuote() {
+  let response = await fetch("https://the-one-api.dev/v2/quote", {
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const data = await response.json();
+  console.log(data);
 }
 
 getCharacter();
-
-
+getQuote();
